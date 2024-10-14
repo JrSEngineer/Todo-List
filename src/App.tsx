232ba1ceac2048cce4fdca68104c851b2059a624
Jr-Oliveira-Silva <jr.oliveira.dev@gmail.com>
2024-10-14 18:56:55 -0300

    :sparkles: Starting List Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   return (
-    <></>
+    <main className='app' >
+      <div className="todo-list">
+        <h2>Minha Lista</h2>
+        <ul>
+          <li>Tarefa n</li>
+          <li>Tarefa n</li>
+          <li>Tarefa n</li>
+        </ul>
+      </div>
+    </main>
   )
 }
 
