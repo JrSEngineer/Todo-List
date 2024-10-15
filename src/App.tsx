fb6169e4ed50fc0105f98c5b03fbea2416e234b6
Jr-Oliveira-Silva <jr.oliveira.dev@gmail.com>
2024-10-14 23:23:06 -0300

    :art: Scaffolding UI.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,72 @@
-import './App.css'
+import "./App.css"
 
 function App() {
   return (
-    <main className='app' >
+    <main className="app" >
       <div className="todo-list">
-        <h2>Minha Lista</h2>
-        <ul>
-          <li>Tarefa n</li>
-          <li>Tarefa n</li>
-          <li>Tarefa n</li>
-        </ul>
+        <div className="list-content">
+          <div className="list-header">
+            <h2>Minha Lista</h2>
+            <p> Veja o que há para fazer. Sua lista contém todas as suas tarefas, e o progresso de todas elas.</p>
+            <div className="input-section">
+              <input />
+              <div className="input-actions">
+                <button>X</button>
+                <button>X</button>
+              </div>
+            </div>
+          </div>
+          <div className="list">
+            <ul>
+              <li>
+                <div className="task-content">
+                  <h3>Tarefa n</h3>
+                  <p>
+                    Descrição da tarefa, informando detalhes, especificando processos e demais informações referentes à tarefa a ser executada.
+                  </p>
+                </div>
+                <div className="task-status">
+                  <button>Iniciar</button>
+                  <div className="status">
+                    <p>Status</p>
+                    <div className="status-indicator"></div>
+                  </div>
+
+                </div>
+              </li>
+              <li>
+                <div className="task-content">
+                  <h3>Tarefa n</h3>
+                  <p>
+                    Descrição da tarefa, informando detalhes, especificando processos e demais informações referentes à tarefa a ser executada.
+                  </p>
+                </div>
+                <div className="task-status">
+                  <button>Iniciar</button>
+                  <div className="status">
+                    <p>Status</p>
+                    <div className="status-indicator"></div>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <div className="task-content">
+                  <h3>Tarefa n</h3>
+                  <p>
+                    Descrição da tarefa, informando detalhes, especificando processos e demais informações referentes à tarefa a ser executada.
+                  </p>
+                </div>
+                <div className="task-status">
+                  <button>Iniciar</button>
+                  <div className="status">
+                    <p>Status</p>
+                    <div className="status-indicator"></div>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </main>
   )
