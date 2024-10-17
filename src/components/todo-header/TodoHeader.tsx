b7c11c035d4465e8da8d3fab13fc6478e5142017
JrSEngineer <jr.s.engineer@gmail.com>
2024-10-17 20:30:35 -0300

    :hammer: Creating todo-header component.

---
+++ src/components/todo-header/TodoHeader.tsx
@@ -0,0 +1,15 @@
+import "./TodoHeader.css";
+import CreateTodoForm from "../create-todo-form/CreateTodoForm";
+
+export default function TodoHeader() {
+    return (<>
+        <div className="list-header">
+            <h2>Minha Lista</h2>
+            <h3> Veja o que há para fazer. Sua lista contém todas as suas tarefas, e o progresso de todas elas.</h3>
+            <p>Voce também pode adicionar novas tarefas.</p>
+            <h4>Adicionar Tarefa</h4>
+                <CreateTodoForm />
+            
+        </div>
+    </>);
+}
