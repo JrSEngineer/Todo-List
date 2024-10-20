5045237cffd6804105eb6fa7aef0a036cc10c171
JrSEngineer <jr.s.engineer@gmail.com>
2024-10-20 12:44:52 -0300

    :rocket: Finishing porcentage feature.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useForm } from "react-hook-form";
 import { useEffect, useState } from "react";
 import { TodoLoading } from "./components/loading/TodoLoading";
 import TodoButton from "./components/todo-button/TodoButton";
-import { Check } from "lucide-react";
 import { TodoCard } from "./components/todo-card/TodoCard";
 import { Todo } from "./models/todo";
 import "./App.css";
@@ -12,10 +11,17 @@ type CreateTodo = {
   description: string;
 }
 
+type TodoPorcentage = {
+  waiting: number;
+  inProgress: number;
+  completed: number;
+}
+
 
 export default function App() {
-  const { register, reset, handleSubmit } = useForm<CreateTodo>();
   const [todoList, setTodoList] = useState<Todo[]>([]);
+  const [todoPorcentage, setTodoPorcentage] = useState<TodoPorcentage>();
+  const { register, reset, handleSubmit } = useForm<CreateTodo>();
   var [loading, setLoading] = useState<boolean>(false);
   var [todoListModified, setTodoListModifiedState] = useState<boolean>(false);
 
@@ -30,11 +36,15 @@ export default function App() {
         alert("Falha ao obter lista de tarefas!");
         setLoading(false);
       }
-      const data: Todo[] = await response.json();
+
+      const responseJson = await response.json();
+      const data: Todo[] = responseJson["toDos"];
+      const porcentage: TodoPorcentage = responseJson["porcentage"];
 
       data.reverse();
 
       setTodoList(data);
+      setTodoPorcentage(porcentage);
 
       setLoading(false);
     } catch (error) {
@@ -148,49 +158,63 @@ export default function App() {
 
   return (
     <main className="app" >
-      <div className="container">
-        <div className="list-header">
-          <h2>Minha Lista</h2>
-          <h3> Veja o que há para fazer. Sua lista contém todas as suas tarefas, e o progresso de todas elas.</h3>
-          <p>Voce também pode adicionar novas tarefas.</p>
-          <h4>Adicionar Tarefa</h4>
-          <div className="create-todo-form">
-            <form onSubmit={handleSubmit(createNewTodo)}>
-              <div>
-                <label htmlFor="title">Título</label>
-                <input className="todo-input" {...register("title")} />
-              </div>
-              <div>
-                <label htmlFor="description">Descrição</label>
-                <input className="todo-input" {...register("description")} />
-              </div>
-              <TodoButton text="Salvar" buttonType="submit" />
-            </form>
+      <div className="app-container">
+        <div className="container">
+          <div className="list-header">
+            <h2>Minha Lista</h2>
+            <h3> Veja o que há para fazer. Sua lista contém todas as suas tarefas, e o progresso de todas elas.</h3>
+            <p>Voce também pode adicionar novas tarefas.</p>
+            <h4>Adicionar Tarefa</h4>
+            <div className="create-todo-form">
+              <form onSubmit={handleSubmit(createNewTodo)}>
+                <div>
+                  <label htmlFor="title">Título</label>
+                  <input className="todo-input" {...register("title")} />
+                </div>
+                <div>
+                  <label htmlFor="description">Descrição</label>
+                  <input className="todo-input" {...register("description")} />
+                </div>
+                <TodoButton text="Salvar" buttonType="submit" />
+              </form>
+            </div>
+          </div>
+          <div className="todo-list">
+            {loading ?
+              <TodoLoading /> :
+              <ul>
+                {todoList.length > 0 ? todoList.map((todo) => {
+                  return (<TodoCard
+                    todo={todo}
+                    initTodo={async () => initTodo(todo)}
+                    finishTodo={async () => finishTodo(todo)}
+                    deleteTodo={async () => deleteTodo(todo)}
+                  />)
+                }) :
+                  <div className="empty-list">
+                    <h4>Você ainda não possui nenhuma tarefa registrada.</h4>
+                    <p>Crie suas primeiras tarefas e se organize</p>
+                  </div>}
+              </ul>
+            }
           </div>
         </div>
-        <div className="todo-list">
-          {loading ?
-            <TodoLoading /> :
-            <ul>
-              {todoList.length > 0 ? todoList.map((todo) => {
-                return (<TodoCard
-                  todo={todo}
-                  initTodo={async () => initTodo(todo)}
-                  finishTodo={async () => finishTodo(todo)}
-                  deleteTodo={async () => deleteTodo(todo)}
-                />)
-              }) :
-                <div className="empty-list">
-                  <h4>Você ainda não possui nenhuma tarefa registrada.</h4>
-                  <p>Crie suas primeiras tarefas e se organize</p>
-                </div>}
-            </ul>
-          }
-        </div>
       </div>
-      <div className="progress">
-        <p>Porcentagem %</p>
-        <div className="progress-bar"></div>
+      <div className="progress-bar-section">
+        <div className="progress">
+          <p>Não Iniciadas {todoPorcentage?.waiting} %</p>
+          <div className="waiting-progress-bar">
+            <div className="waiting-value" style={{ width: `${todoPorcentage?.waiting}%` }}></div>
+          </div>
+          <p>Em Andamento {todoPorcentage?.inProgress} %</p>
+          <div className="inProgress-progress-bar">
+            <div className="inProgress-value" style={{ width: `${todoPorcentage?.inProgress}%` }}></div>
+          </div>
+          <p>Concluídas {todoPorcentage?.completed} %</p>
+          <div className="completed-progress-bar">
+            <div className="completed-value" style={{ width: `${todoPorcentage?.completed}%` }}></div>
+          </div>
+        </div>
       </div>
     </main>
   )
